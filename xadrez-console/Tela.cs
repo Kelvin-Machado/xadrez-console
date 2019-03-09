bf2b85b351e7c255f7668d036bccae093a90f144
Kelvin-Machado <kelvinmachado@live.com>
2019-03-08 21:13:35 -0300

    Fazendo os primeiros testes de movimentação

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -9,7 +10,7 @@ namespace xadrez_console
         {
             for (int i = 0; i < tab.linhas; i++)
             {
-                Console.Write(8 - i + "");
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -28,6 +29,14 @@ namespace xadrez_console
             Console.WriteLine("  a b c d e f g h");
         }
 
+        internal static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
+        }
+
         public static void imprimirPeca(Peca peca)
         {
             if (peca.cor == Cor.Branca)
@@ -37,7 +46,7 @@ namespace xadrez_console
             else
             {
                 ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write(peca);
                 Console.ForegroundColor = aux;
             }
