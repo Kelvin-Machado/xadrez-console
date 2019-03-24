2f00794f34656ac104dda5d93b1bde25e6b2fb5d
Kelvin-Machado <kelvinmachado@live.com>
2019-03-24 19:20:26 -0300

    jogada especial promoção

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -142,6 +142,27 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em Xeque!");
             }
+
+
+            Peca p = tab.peca(destino);
+
+
+            // jogadaespecial promocao
+            if (p is Peao)
+            {
+                if (p.cor == Cor.Branca && destino.linha == 0 || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(p.cor, tab);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
+
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 Xeque = true;
@@ -161,7 +182,7 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+
 
             // #jogadaespecial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
