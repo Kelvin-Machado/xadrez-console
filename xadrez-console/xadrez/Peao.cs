4b64c02771693009b52fcc609e3c10c5165fa7e4
Kelvin-Machado <kelvinmachado@live.com>
2019-03-24 17:36:40 -0300

    Jogada especial roque pequeno e roque grande

--- xadrez-console/xadrez/Peao.cs
+++ xadrez-console/xadrez/Peao.cs
@@ -5,7 +5,7 @@ namespace xadrez
 {
     class Peao : Peca
     {
-        public Peao(Cor cor, Tabuleiro tab, PartidaDeXadrez partidaDeXadrez) : 
+        public Peao(Cor cor, Tabuleiro tab, PartidaDeXadrez partidaDeXadrez) :
         base(cor, tab)
         {
         }
@@ -16,42 +16,43 @@ namespace xadrez
         }
 
 
-        private bool existeInimigo (Posicao pos)
+        private bool existeInimigo(Posicao pos)
         {
             Peca p = tab.peca(pos);
             return p != null && p.cor != cor;
         }
 
-        private bool livre (Posicao pos)
+        private bool livre(Posicao pos)
         {
             return tab.peca(pos) == null;
         }
 
         public override bool[,] movimentosPossiveis()
         {
-            bool[,] mat = new bool [tab.linhas, tab.colunas];
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
 
             Posicao pos = new Posicao(0, 0);
 
             if (cor == Cor.Branca)
             {
                 pos.definirValores(posicao.linha - 1, posicao.coluna);
-                if (tab.posicaoValida(pos)&& livre(pos))
+                if (tab.posicaoValida(pos) && livre(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha - 2, posicao.coluna);
-                if (tab.posicaoValida(pos) && livre(pos) && qteMovimentos == 0)
+                Posicao p2 = new Posicao(posicao.linha - 1, posicao.coluna);
+                if (tab.posicaoValida(p2) && livre(p2) && tab.posicaoValida(pos) && livre(pos) && qteMovimentos == 0)
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
-                pos.definirValores(posicao.linha - 1, posicao.coluna -1);
-                if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos))
+                pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
-                pos.definirValores(posicao.linha - 1, posicao.coluna +1);
-                if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos))
+                pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
@@ -64,17 +65,18 @@ namespace xadrez
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha + 2, posicao.coluna);
-                if (tab.posicaoValida(pos) && livre(pos) && qteMovimentos == 0)
+                Posicao p2 = new Posicao(posicao.linha + 1, posicao.coluna);
+                if (tab.posicaoValida(p2) && livre(p2) && tab.posicaoValida(pos) && livre(pos) && qteMovimentos == 0)
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
-                if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos))
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
-                if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos))
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
